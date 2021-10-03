096d34f1e79cdd7c8f31719283bb69b4e37f1617
João Bispo <joaobispo2077@gmail.com>
2021-10-03 15:22:12 -0300

    feat: add styles into page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 
-import { Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading, VStack } from '@chakra-ui/react';
 
 import { OptionsBar } from '../components/OptionsBar';
 import { Form } from '../components/Form';
@@ -8,9 +8,11 @@ import { Panel } from '../components/Panel';
 
 const Home: NextPage = () => {
   return (
-    <Flex>
-      <Heading>Hello World</Heading>
-      <OptionsBar />
+    <Flex w="100vw" h="100vh" bgColor="gray.200" padding="3.5rem">
+      <VStack>
+        <Heading color="primary.500">Q</Heading>
+        <OptionsBar />
+      </VStack>
       <Form />
       <Panel />
     </Flex>
