2bf8b732f9f823f4a8e6d5ec0086861af7686366
João Bispo <joaobispo2077@gmail.com>
2021-10-03 14:37:21 -0300

    feat: import components to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next';
 
-import { Heading } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
+
+import { OptionsBar } from '../components/OptionsBar';
+import { Form } from '../components/Form';
+import { Panel } from '../components/Panel';
 
 const Home: NextPage = () => {
-  return <Heading>Hello world!!</Heading>;
+  return (
+    <Flex>
+      <Heading>Hello World</Heading>
+      <OptionsBar />
+      <Form />
+      <Panel />
+    </Flex>
+  );
 };
 
 export default Home;
