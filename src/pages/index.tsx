240a41cff31b74d4a120fd99726728a3439bf44a
João Bispo <joaobispo2077@gmail.com>
2021-10-10 10:37:05 -0300

    feat: add title into home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 
+import Head from 'next/head';
 import { Flex, Heading, VStack } from '@chakra-ui/react';
 
 import { OptionsBar } from '../components/OptionsBar';
@@ -9,6 +10,9 @@ import { Panel } from '../components/Panel';
 const Home: NextPage = () => {
   return (
     <Flex w="100vw" h="100vh" bgColor="gray.200" padding="3.5rem">
+      <Head>
+        <title>QR Code - Easy share</title>
+      </Head>
       <VStack>
         <Heading color="primary.500">Q</Heading>
         <OptionsBar />
