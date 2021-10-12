b90b2a676ebf0e00107b6ad600a6ea9c348c6b76
João Bispo <joaobispo2077@gmail.com>
2021-10-12 15:03:46 -0300

    feat: handle url form with mocked onSubmit function

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-import { VStack, Input, Button, Icon, Text, Flex } from '@chakra-ui/react';
+import {
+  VStack,
+  Input,
+  Button,
+  Icon,
+  Text,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+} from '@chakra-ui/react';
 import { FiRefreshCcw } from 'react-icons/fi';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Inputs = {
+  url: string;
+};
 
 export const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <Flex
       as="form"
@@ -13,22 +34,31 @@ export const Form = () => {
       flex="2"
       direction="column"
       padding="2rem"
+      onSubmit={handleSubmit(onSubmit)}
     >
-      <VStack>
-        <Input
-          w="100%"
-          colorScheme="yellow"
-          placeholder="Insira o link aqui"
-          _placeholder={{
-            color: 'primary.500',
-            fontSize: '1.2rem',
-            fontWeight: 'bold',
-          }}
-        />
-        <Text fontSize="sm" color="gray.500">
-          Gere seu QR Code e compartilhe facilmente o seu link.
-        </Text>
-      </VStack>
+      <FormControl isInvalid={!!errors.url}>
+        <VStack align="flex-start">
+          <Input
+            type="url"
+            w="100%"
+            colorScheme="yellow"
+            placeholder="Insira o link aqui..."
+            _placeholder={{
+              color: 'primary.500',
+              fontSize: '1.2rem',
+              fontWeight: 'bold',
+            }}
+            {...register('url', { required: true })}
+            required
+          />
+          {!!errors.url && (
+            <FormErrorMessage>{errors.url.message}</FormErrorMessage>
+          )}
+          <Text fontSize="sm" color="gray.500">
+            Gere seu QR Code e compartilhe facilmente o seu link.
+          </Text>
+        </VStack>
+      </FormControl>
       <Flex mt="5rem">
         <Button
           type="submit"
